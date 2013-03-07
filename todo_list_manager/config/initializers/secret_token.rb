13b65ed31babd47b913a13bda34b64648cb8ade1
Felipe Castro <felipe@Ubuntu-Dell-System-XPS-L502X.(none)>
2013-03-07 02:03:17 -0300

    Created todo_list_manager project

---
+++ todo_list_manager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoListManager::Application.config.secret_token = '510ba2415940c033544433e1f6e20f03a949e0e8bf448766a06c1267b7442557c2005cdfbadc356cf3eeec72fe475db46a9aea01744f7ee81d9b298d4a095e83'
